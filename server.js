77a1713f350247fcbe13ba4f50a46a75f7e474cc
joel <joelbenitezdonari@gmail.com>
2024-05-25 11:39:08 +0200

    prompt user

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const replicate = new Replicate({
 });
 
 // Función para llamar a la API de Replicate
-async function replicateRun(prompt) {
-
+async function replicateRun(UserPrompt) {
+   
     const output = await replicate.run(
         "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
         {
           input: {
             width: 768,
             height: 768,
-            prompt: "an astronaut riding a horse on mars, hd, dramatic lighting",
+            prompt: UserPrompt,
             scheduler: "K_EULER",
             num_outputs: 1,
             guidance_scale: 7.5,
